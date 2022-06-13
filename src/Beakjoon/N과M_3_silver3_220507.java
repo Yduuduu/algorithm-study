457ba3002255676814ac76c7369eda80005241c1
Yduuduu <hou_o@naver.com>
2022-06-13 23:59:02 +0900

    20220613

---
+++ src/Beakjoon/N과M_3_silver3_220507.java
@@ -0,0 +1,39 @@
+package Beakjoon;
+
+import java.util.Scanner;
+
+public class N°úM_3_silver3_220507 {
+	public static int[] arr;
+	public static int N, M;
+	public static StringBuilder sb = new StringBuilder();
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		N = in.nextInt();
+		M = in.nextInt();
+ 
+		arr = new int[M];
+        
+		dfs(0);
+		System.out.print(sb);
+ 
+	}
+ 
+	public static void dfs(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[i] + " ");
+			}
+			sb.append('\n');
+			return;
+		}
+ 
+		for (int i = 1; i <= N; i++) {
+			arr[depth] = i;
+			dfs(depth + 1);
+		}
+	}
+
+}
